f710a18a6b9a9239e7fef1a456138d374d721a1e
rimlo <145866237+rimlo@users.noreply.github.com>
2023-09-27 10:49:32 +0700

    Update test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("Hello, world!");
+
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(3, 5);
+console.log("3 + 5 =", result);
+
+const fruits = ["apple", "banana", "cherry"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
